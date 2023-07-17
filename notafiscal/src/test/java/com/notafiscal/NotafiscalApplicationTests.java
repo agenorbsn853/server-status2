a97a2c4008be5863f7161d7d3b67d88f84637426
agenor barbosa <agenorbsn853@gmail.com>
2023-07-17 19:09:40 -0300

    commit appnotefiscal

---
+++ notafiscal/src/test/java/com/notafiscal/NotafiscalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.notafiscal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotafiscalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
